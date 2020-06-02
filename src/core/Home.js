668f5b2f51bcd80b0e26a089df654277062c2b74
aditya tiwai <adityat599@gmail.com>
2020-06-02 23:09:08 +0530

    ADDED TO FRONT END

--- src/core/Home.js
+++ src/core/Home.js
@@ -2,21 +2,21 @@ import React from 'react';
 import "../styles.css"
 import {API} from "../backend";
 import Base from "../core/Base";
+import Card from './Card';
+
 
 export default function Home() {
-    console.log("API IS",API);
-    
-    return (
+  return (
         <Base title="Home Page" description="Welcome to the T-shirt Store">
            <div className="row">
            <div className="col-4">
-           <button className="btn btn-success">Test</button>
+           <button className="btn btn-success">{Card()}</button>
            </div>
            <div className="col-4">
-           <button className="btn btn-success">Test</button>
+           <button className="btn btn-success">{Card()}</button>
            </div>
            <div className="col-4">
-           <button className="btn btn-success">Test</button>
+           <button className="btn btn-success">{Card()}</button>
 
             </div>
            </div>
