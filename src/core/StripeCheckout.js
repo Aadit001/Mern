3461df4df17545d96da059b05e9a7f77e6dc39ee
aditya tiwai <adityat599@gmail.com>
2020-06-21 16:16:29 +0530

    payment gatwat integratio with test apis

--- src/core/StripeCheckout.js
+++ src/core/StripeCheckout.js
@@ -2,8 +2,9 @@ import React, {useEffect,useState} from 'react';
 import {isAuthenticated} from '../auth/helper';
 import {cartEmpty,loadCart} from './helper/cartHelper';
 import { Link } from 'react-router-dom';
-
-
+import StripeCheckoutButton from 'react-stripe-checkout';
+import {API} from "../backend";
+import {createOrder} from "./helper/orderHelper"
 
 const  StripeCheckout = ({products, setReload = f => f ,reload= undefined})=> {
 
@@ -20,14 +21,44 @@ const userId = isAuthenticated() && isAuthenticated().user._id
 const getFinalAmount= () => {
 let amount=0;
 products.map(p =>{
-amount = amount + p.price;
+amount = amount + p.price ;
 });
 return amount;
 };
 
+const makePayment = (token) =>{
+   const body = {
+       token,
+       products
+   }
+   const headers = {
+       "Content-Type":"application/json"
+   }
+   return fetch(`${API}/stripepayment`,{
+       method:"POST",
+       headers,
+       body:JSON.stringify(body)
+   }).then(response =>{
+console.log(response);
+//we can call further methods here
+const {status} = response;
+console.log("STATUS",status);
+cartEmpty()
+  }).catch(error => console.log(error));
+};
+
 const showStripeButton =() =>{
 return isAuthenticated() ? (
-    <button className="btn btn-success">Pay Now</button>
+    <StripeCheckoutButton
+    stripeKey="pk_test_51Gs0hzBmeoKgJhNub9YMbNSPPzV8Hr6BGW4QkbxIEJc4t50K0fK34hNs58T3cpuBYkhX4i7D4AhmrnIWJioPlPFU00K9dDbPVQ"
+    token={makePayment}
+    amount={getFinalAmount()*100}
+    name="Buy T-shirts"
+    shippingAddress
+    billingAddress
+    >
+    <button className="btn btn-success">Pay with Stripe</button>
+    </StripeCheckoutButton>
 ) : (
   <Link to ="/signin">  
   <button className="btn btn-warning">Signin</button>
